b5b18c4059e91ca8135869977f2d0333b62f73e9
Filip Mazurek <22063152+filipmazurek@users.noreply.github.com>
2021-11-09 09:43:40 -0500

    Adds Duke raft project examples

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Implementing the Raft protocol in Python.
 
 [MIT Raft Lab](https://pdos.csail.mit.edu/6.824/labs/lab-raft.html)
 
-[Random Repo](https://github.com/nsiregar/mit-go/tree/master/src) that has MIT's raft code
+[MIT Code Repo](https://github.com/nsiregar/mit-go/tree/master/src) that has MIT's raft code
+
+[Duke Raft Lab](https://courses.cs.duke.edu/fall18/compsci310/projects/p3.html)
+
+[Duke Code Repo](https://github.com/maschreiber/CS310/tree/master/p3)
 
 [Raft description](https://raft.github.io/)
---
+++ p3/src/edu/duke/raft/LeaderMode.java
@@ -0,0 +1,59 @@
+package edu.duke.raft;
+
+public class LeaderMode extends RaftMode {
+  public void go () {
+    synchronized (mLock) {
+      int term = 0;
+      System.out.println ("S" + 
+			  mID + 
+			  "." + 
+			  term + 
+			  ": switched to leader mode.");
+    }
+  }
+  
+  // @param candidate’s term
+  // @param candidate requesting vote
+  // @param index of candidate’s last log entry
+  // @param term of candidate’s last log entry
+  // @return 0, if server votes for candidate; otherwise, server's
+  // current term
+  public int requestVote (int candidateTerm,
+			  int candidateID,
+			  int lastLogIndex,
+			  int lastLogTerm) {
+    synchronized (mLock) {
+      int term = mConfig.getCurrentTerm ();
+      int vote = term;
+      return vote;
+    }
+  }
+  
+
+  // @param leader’s term
+  // @param current leader
+  // @param index of log entry before entries to append
+  // @param term of log entry before entries to append
+  // @param entries to append (in order of 0 to append.length-1)
+  // @param index of highest committed entry
+  // @return 0, if server appended entries; otherwise, server's
+  // current term
+  public int appendEntries (int leaderTerm,
+			    int leaderID,
+			    int prevLogIndex,
+			    int prevLogTerm,
+			    Entry[] entries,
+			    int leaderCommit) {
+    synchronized (mLock) {
+      int term = mConfig.getCurrentTerm ();
+      int result = term;
+      return result;
+    }
+  }
+
+  // @param id of the timer that timed out
+  public void handleTimeout (int timerID) {
+    synchronized (mLock) {
+    }
+  }
+}
